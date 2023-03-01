bd4c29312086a04557e9a28a53c9ff748fe1d84b
Tommy McNeely <thomas.mcneely@davita.com>
2023-03-01 14:41:42 -0700

    feat: re-enable the f5xc fun stuff

--- infra/f5-multi.tf
+++ infra/f5-multi.tf
@@ -133,54 +133,54 @@ resource "google_compute_firewall" "f5_ip_ranges_ingress_dmz" {
 }
 
 
-# resource "google_compute_image" "f5xc_multi" {
-#   name    = var.f5_machine_image_multi
-#   project = var.gcp_project_id
-#   family  = "f5xc-ce"
-#   raw_disk {
-#     source = "https://storage.googleapis.com/ves-images/${var.f5_machine_image_multi}.tar.gz"
-#   }
-# }
+resource "google_compute_image" "f5xc_multi" {
+  name    = var.f5_machine_image_multi
+  project = var.gcp_project_id
+  family  = "f5xc-ce"
+  raw_disk {
+    source = "https://storage.googleapis.com/ves-images/${var.f5_machine_image_multi}.tar.gz"
+  }
+}
 
-# module "f5_ce_multi" {
-#   # source = "github.com/cklewar/f5-xc-modules//f5xc/ce/gcp?ref=0aaa5ca" # main (previous commit)
-#   source = "github.com/cklewar/f5-xc-modules//f5xc/ce/gcp?ref=853b86c" # main
-#   # source = "github.com/tjm/f5-xc-modules//f5xc/ce/gcp?ref=7d2bb4d" # fix/no-public-ips
-#   # source                         = "../../F5/f5-xc-modules/f5xc/ce/gcp"
-#   has_public_ip = false # 0.11.18
-#   # use_public_ip                  = false
-#   f5xc_ce_gateway_multi_node     = false # this is broken when true
-#   gcp_region                     = var.gcp_region
-#   gcp_service_account_email      = google_service_account.f5xc.email
-#   fabric_subnet_outside          = "" # empty string - do not create
-#   fabric_subnet_inside           = "" # empty string - do not create
-#   existing_fabric_subnet_outside = google_compute_subnetwork.dmz.self_link
-#   existing_fabric_subnet_inside  = google_compute_subnetwork.main.self_link
-#   network_name                   = "" # unused
-#   instance_name                  = "${var.name_prefix}-${var.env}-f5xc-multi-${random_id.suffix.hex}"
-#   ssh_username                   = "centos"
-#   machine_type                   = var.machine_type
-#   machine_image                  = google_compute_image.f5xc_multi.name
-#   machine_disk_size              = var.machine_disk_size
-#   ssh_public_key                 = file(var.ssh_public_key_file)
-#   host_localhost_public_name     = "vip"
-#   f5xc_tenant                    = var.f5xc_tenant
-#   f5xc_api_url                   = var.f5xc_api_url
-#   f5xc_namespace                 = var.f5xc_namespace
-#   f5xc_api_token                 = var.f5xc_api_token
-#   f5xc_token_name                = "${var.name_prefix}-${var.env}-multi-${random_id.suffix.hex}"
-#   f5xc_fleet_label               = var.f5xc_fleet_label
-#   f5xc_cluster_latitude          = var.cluster_latitude
-#   f5xc_cluster_longitude         = var.cluster_longitude
-#   f5xc_ce_gateway_type           = "ingress_egress_gateway"
-#   instance_tags                  = ["f5xc"]
+module "f5_ce_multi" {
+  # source = "github.com/cklewar/f5-xc-modules//f5xc/ce/gcp?ref=0aaa5ca" # main (previous commit)
+  source = "github.com/cklewar/f5-xc-modules//f5xc/ce/gcp?ref=853b86c" # main
+  # source = "github.com/tjm/f5-xc-modules//f5xc/ce/gcp?ref=7d2bb4d" # fix/no-public-ips
+  # source                         = "../../F5/f5-xc-modules/f5xc/ce/gcp"
+  has_public_ip = false # 0.11.18
+  # use_public_ip                  = false
+  f5xc_ce_gateway_multi_node     = false # this is broken when true
+  gcp_region                     = var.gcp_region
+  gcp_service_account_email      = google_service_account.f5xc.email
+  fabric_subnet_outside          = "" # empty string - do not create
+  fabric_subnet_inside           = "" # empty string - do not create
+  existing_fabric_subnet_outside = google_compute_subnetwork.dmz.self_link
+  existing_fabric_subnet_inside  = google_compute_subnetwork.main.self_link
+  network_name                   = "" # unused
+  instance_name                  = "${var.name_prefix}-${var.env}-f5xc-multi-${random_id.suffix.hex}"
+  ssh_username                   = "centos"
+  machine_type                   = var.machine_type
+  machine_image                  = google_compute_image.f5xc_multi.name
+  machine_disk_size              = var.machine_disk_size
+  ssh_public_key                 = file(var.ssh_public_key_file)
+  host_localhost_public_name     = "vip"
+  f5xc_tenant                    = var.f5xc_tenant
+  f5xc_api_url                   = var.f5xc_api_url
+  f5xc_namespace                 = var.f5xc_namespace
+  f5xc_api_token                 = var.f5xc_api_token
+  f5xc_token_name                = "${var.name_prefix}-${var.env}-multi-${random_id.suffix.hex}"
+  f5xc_fleet_label               = var.f5xc_fleet_label
+  f5xc_cluster_latitude          = var.cluster_latitude
+  f5xc_cluster_longitude         = var.cluster_longitude
+  f5xc_ce_gateway_type           = "ingress_egress_gateway"
+  instance_tags                  = ["f5xc"]
 
-#   depends_on = [
-#     google_compute_router_nat.dmz
-#   ]
+  depends_on = [
+    google_compute_router_nat.dmz
+  ]
 
-#   providers = {
-#     google   = google.project_bound
-#     volterra = volterra
-#   }
-# }
+  providers = {
+    google   = google.project_bound
+    volterra = volterra
+  }
+}
